8a7beb5b2adf6285094f0a850455e0febfc09721
arpanosso <arpanosso@yahoo.com.br>
2024-12-13 12:01:58 -0300

    lista 06 - diagnóstico

--- data-raw/faxina.R
+++ data-raw/faxina.R
@@ -1,4 +1,4 @@
-# FAXINA DE DADOS
+# FAXINA DE DADOS - geomorfologia
 library(tidyverse)
 library(janitor)
 library(readxl)
@@ -6,3 +6,8 @@ read_xlsx("data-raw/geomorfologia.xlsx") |>
   clean_names() -> df
 glimpse(df)
 write_rds(df,"data/geomorfologia.rds")
+
+# FAXINA DE DADOS - macieira
+read_xlsx("data-raw/dadosprodmacieira.xlsx") |>
+  clean_names() -> df
+write_rds(df,"data/prodmacieira.rds")
